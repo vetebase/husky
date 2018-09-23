1d9f8c6e714d886aafc2eb013f549b8d59ac5164
Lark Mullins <larkmullins@gmail.com>
2018-09-23 15:05:55 -0400

    added New, Middleware, AfterMiddleware, and BeforeMiddleware funcs

--- husky.go
+++ husky.go
@@ -1,5 +1,6 @@
 package husky
 
+// Husky struct holds router and context for framework
 type Husky struct {
 	AfterMiddleware  []MiddlewareHandler
 	BeforeMiddleware []MiddlewareHandler
@@ -13,3 +14,29 @@ type Handler func(Context) error
 
 // MiddlewareHandler defines a function to process middleware
 type MiddlewareHandler func(Handler) Handler
+
+// New creates a new service
+func New() (h *Husky) {
+	return &Husky{
+		Router: new(Router),
+	}
+}
+
+// After adds a handler to be executed after the route handler
+func (h *Husky) After(middleware ...MiddlewareHandler) {
+	for i := 0; i < len(middleware); i++ {
+		h.AfterMiddleware = append(h.AfterMiddleware, middleware[i])
+	}
+}
+
+// Before adds a handler to be executed before the route handler
+func (h *Husky) Before(middleware ...MiddlewareHandler) {
+	for i := 0; i < len(middleware); i++ {
+		h.BeforeMiddleware = append(h.BeforeMiddleware, middleware[i])
+	}
+}
+
+// Middlware adds a handler to be executed before the route handler
+func (h *Husky) Middlware(middleware MiddlewareHandler) {
+	h.Middleware = append(h.Middleware, middleware)
+}
