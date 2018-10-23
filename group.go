09a4e9ce4b67155c82e4d970034725faeff42237
Lark Mullins <larkmullins@gmail.com>
2018-10-22 19:26:18 -0500

    added rest of http verbs for adding handlers to route group

--- group.go
+++ group.go
@@ -12,6 +12,26 @@ func (g *Group) GET(endpoint string, handler Handler, middleware ...MiddlewareHa
 	g.add("GET", endpoint, handler, middleware)
 }
 
+// POST adds a HTTP POST method to the group
+func (g *Group) POST(endpoint string, handler Handler, middleware ...MiddlewareHandler) {
+	g.add("POST", endpoint, handler, middleware)
+}
+
+// PATCH adds a HTTP PATCH method to the group
+func (g *Group) PATCH(endpoint string, handler Handler, middleware ...MiddlewareHandler) {
+	g.add("PATCH", endpoint, handler, middleware)
+}
+
+// PUT adds a HTTP PUT method to the group
+func (g *Group) PUT(endpoint string, handler Handler, middleware ...MiddlewareHandler) {
+	g.add("PUT", endpoint, handler, middleware)
+}
+
+// DELETE adds a HTTP DELETE method to the group
+func (g *Group) DELETE(endpoint string, handler Handler, middleware ...MiddlewareHandler) {
+	g.add("DELETE", endpoint, handler, middleware)
+}
+
 func (g *Group) add(verb string, endpoint string, handler Handler, middleware []MiddlewareHandler) {
 	g.Husky.add(verb, g.Prefix+endpoint, handler, middleware)
 }
